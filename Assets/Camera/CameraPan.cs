d5d23e957b4cb77f027398940360659b533bf81d
JPPerkins <jcperkins482@gmail.com>
2018-10-30 16:34:27 -0400

    Finish

---
+++ Assets/Camera/CameraPan.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPan : MonoBehaviour {
+
+	private GameObject player;
+	void Start() {
+		player = GameObject.FindGameObjectWithTag("Player");
+	}
+	// Update is called once per frame
+
+	void LateUpdate() {
+		transform.LookAt(player.transform);
+	}
+}
